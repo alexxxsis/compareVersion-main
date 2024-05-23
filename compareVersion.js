bca247cb7fd78b105f7e3cf6fdf0cf6cdb5e9940
alexxxsis <144637900+alexxxsis@users.noreply.github.com>
2024-05-23 09:50:33 +0800

    Update compareVersion.js

--- compareVersion.js
+++ compareVersion.js
@@ -1,21 +1,23 @@
 const compareVersion = (version1, version2) => {
-    if (version1.length === version2.length) {
-        if (parseFloat(version1) === parseFloat(version2)) {
-            return 0;
+    let result = 0;
+    
+    if (version1.slice(0, version1.indexOf('.')) === version2.slice(0, version2.indexOf('.'))) {              
+        if (parseFloat(version1.slice(version1.indexOf('.') + 1)) > version2.slice(version2.indexOf('.') + 1)) {
+            result = 1;
+        }  
+        if (parseFloat(version1.slice(version1.indexOf('.') + 1)) < version2.slice(version2.indexOf('.') + 1)) {
+            result = -1;
         }
-        else if (parseFloat(version1) > parseFloat(version2)) {
-            return 1;
-        } 
-        else {
-            return -1;
-        }
+        
     }
-    else if (version1.length > version2.length) {
-        return 1;
-    } 
-    else {
-        return -1;
+    if (version1.slice(0, version1.indexOf('.')) > version2.slice(0, version2.indexOf('.'))) {
+        result = 1;
+    }     
+    if (version1.slice(0, version1.indexOf('.')) < version2.slice(0, version2.indexOf('.'))) {
+        result = -1;
     }
+    
+    return result;
 };
 
 exports.compareVersion = compareVersion;
